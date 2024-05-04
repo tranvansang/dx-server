ffedf05d1844485f26da12f465c44fd2d40bac88
Sang Tran <t@sang.jp>
2024-05-04 10:10:18 +0900

    Reapply "drop qs dependency"
    This reverts commit 1e928da4a2558e06355e063e76861165b4b13a19.

--- src/body.ts
+++ src/body.ts
@@ -14,7 +14,7 @@ export const getJson = makeDxContext((options?: Partial<BufferBodyOptions>) => j
 export const getRaw = makeDxContext((options?: Partial<BufferBodyOptions>) => rawFromReq(getReq(), options))
 export const getText = makeDxContext((options?: Partial<BufferBodyOptions>) => textFromReq(getReq(), options))
 export const getUrlEncoded = makeDxContext((options?: Partial<BufferBodyOptions>) => urlEncodedFromReq(getReq(), options))
-export const getQuery = makeDxContext((options?: Partial<BufferBodyOptions> & {simplify?: boolean}) => queryFromReq(getReq(), options))
+export const getQuery = makeDxContext((options?: Partial<BufferBodyOptions>) => queryFromReq(getReq(), options))
 
 // to getFile use busboy
 // https://github.com/mscdex/busboy
